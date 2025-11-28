c686fe0a18b9a6639da1426e040241a853a32571
Douglas Lobo <douglascnlobo@icloud.com>
2025-11-28 18:03:51 +0000

    Added camera permissions and dependencies for barcode scanning and camera features

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,11 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.squareup.retrofit2:retrofit:3.0.0")
     implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+    implementation("com.google.mlkit:barcode-scanning:17.3.0")
+    implementation("androidx.camera:camera-core:1.5.1")
+    implementation("androidx.camera:camera-camera2:1.5.1")
+    implementation("androidx.camera:camera-lifecycle:1.2.2")
+    implementation("androidx.camera:camera-video:1.5.1")
+    implementation("androidx.camera:camera-view:1.5.1")
+    implementation("androidx.camera:camera-extensions:1.5.1")
 }
