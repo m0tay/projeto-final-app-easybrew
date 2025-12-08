c564130b22b907619a5a278d5dce988bf1fd76b5
Douglas Lobo <douglascnlobo@icloud.com>
2025-12-08 19:18:21 +0000

    Add Beverage model class and update library versions for CardView and RecyclerView

---
+++ app/src/main/java/pm/easybrew/models/Beverage.kt
@@ -0,0 +1,12 @@
+package pm.easybrew.models
+
+class Beverage {
+    val id: String = ""
+    val name: String = ""
+    val description: String = ""
+    val price: Double = 0.0
+    val image: String = ""
+    val type: String = ""
+    val size: String = ""
+    val preparation: String = ""
+}
